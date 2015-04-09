6d648edb8d988cba6e8023d9d0c979190f6b82aa
Steven Ramirez <netssrmrz@yahoo.com.au>
2015-04-09 17:51:31 +1000

    update

--- src/rs/android/ui/Date_Dialog.java
+++ src/rs/android/ui/Date_Dialog.java
@@ -1,9 +1,7 @@
 package rs.android.ui;
-import java.util.*;
 
 public class Date_Dialog
 implements 
-android.content.DialogInterface.OnClickListener,
 android.app.DatePickerDialog.OnDateSetListener
 {
   public interface On_Date_Set_Listener
@@ -12,13 +10,11 @@ android.app.DatePickerDialog.OnDateSetListener
 	}
 	
 	public android.content.Context ctx;
-	public boolean dlg_ok;
 	public On_Date_Set_Listener on_date_set_listener;
 
 	public Date_Dialog(android.content.Context ctx, On_Date_Set_Listener listener)
 	{
 		this.ctx = ctx;
-		this.dlg_ok = false;
 		this.on_date_set_listener=listener;
 	}
 
@@ -33,26 +29,17 @@ android.app.DatePickerDialog.OnDateSetListener
 		month = rs.android.util.Date.Date_Get_Month(init);
 		day = rs.android.util.Date.Date_Get_Day(init);
 		dlg = new android.app.DatePickerDialog(this.ctx, this, year, month, day);
-		dlg.setButton(android.content.DialogInterface.BUTTON_NEGATIVE, "Cancel", this);
-		dlg.setButton(android.content.DialogInterface.BUTTON_POSITIVE, "Done", this);
 		dlg.show();
 	}
+  
+  public void onDateSet(android.widget.DatePicker v, int year, int month, int day)
+  {
+    java.sql.Date date;
 
-	public void onClick(android.content.DialogInterface dlg, int which)
-	{
-		if (which == android.content.DialogInterface.BUTTON_POSITIVE)
-			this.dlg_ok = true;
-	}
-
-	public void onDateSet(android.widget.DatePicker v, int year, int month, int day)
-	{
-		java.sql.Date date;
-		
-		if (this.dlg_ok && this.on_date_set_listener!=null)
-		{
-			date=rs.android.util.Date.New_Date(year, month+1, day);
-			this.on_date_set_listener.On_Date_Set(date);
-		}
-		this.dlg_ok = false;
+    if (this.on_date_set_listener!=null)
+    {
+      date=rs.android.util.Date.New_Date(year, month+1, day);
+      this.on_date_set_listener.On_Date_Set(date);
+    }
 	}
 }
